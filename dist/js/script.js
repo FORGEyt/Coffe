57b8cf9de455a7817510e700cbe6dcfe655ff249
Oleg <youoleg212@gmail.com>
2022-03-25 13:13:26 +0300

    create new bloks and add some adoptation

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,13 +1,13 @@
-const hamburger = document.querySelector('.hamburger'),
-      menu = document.querySelector('.menu'),
-      closeElem = document.querySelector('.menu__close');
+const hamburger = document.querySelector('.header__hamburger'),
+      menu = document.querySelector('.header__overlay'),
+      closeElem = document.querySelector('.header__overlay_close');
 
 hamburger.addEventListener('click', () => {
-    menu.classList.add('active');
+    menu.classList.add('view');
 });
 
 closeElem.addEventListener('click', () => {
-    menu.classList.remove('active');
+    menu.classList.remove('view');
 });
 
 const counters = document.querySelectorAll('.skills__ratings-counter'),
